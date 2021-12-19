0627e39023a059bd3548e72dd7349216b8358877
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2021-12-19 20:45:07 +0530

    Add files via upload

---
+++ Reversestring.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverseString(vector<char> &s)
+{
+    int start = 0;
+    int end = s.size() - 1;
+    while (start < end)
+    {
+        swap(s[start++], s[end--]);
+    }
+}
