367116cfe268b7540323a1604a27b2302c4f8a30
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-02-04 22:30:00 +0530

    Add files via upload

---
+++ Number_of_Strings_That_Appear_as_Substrings_in_Word.cpp
@@ -0,0 +1,21 @@
+/*Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
+
+A substring is a contiguous sequence of characters within a string.*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count=0;
+        
+        for(int i=0;i<patterns.size();i++){
+            
+            if(word.find(patterns[i])!=-1)
+                count++;
+        }
+            return count;
+    }
+};
