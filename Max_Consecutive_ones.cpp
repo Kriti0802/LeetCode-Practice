8a94567c64c1e00de2763e80c8ad9be196f57a19
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-02-11 18:58:50 +0530

    Add files via upload

---
+++ Max_Consecutive_ones.cpp
@@ -0,0 +1,25 @@
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+             count++;
+              ans=max(count,ans);
+            }
+            else{
+                count=0;
+            }
+        }
+        return ans;
+    }
+};
