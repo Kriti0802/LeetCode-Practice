0fe9859c5eb6d90b8f358531db723cad513cf58d
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-02-28 00:43:05 +0530

    Add files via upload

---
+++ Combine_Two_Tables.sql
@@ -0,0 +1,4 @@
+/*Write an SQL query to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.*/
+
+
+select p.firstName,p.lastName,a.city,a.state from person p left outer join address a on p.personId=a.personId; 
