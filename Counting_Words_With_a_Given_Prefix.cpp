32c78729b74c5625935a11573f847b92baf09eb7
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-04-15 11:52:03 +0530

    Add files via upload

--- Counting_Words_With_a_Given_Prefix.cpp
+++ Counting_Words_With_a_Given_Prefix.cpp
@@ -4,18 +4,24 @@ Return the number of strings in words that contain pref as a prefix.
 
 A prefix of a string s is any leading contiguous substring of s.*/
 
-#include <iostream>
-#include <bits/stdc++.h>
-#include <vector>
+#include<bits\stdc++.h>
+#include<vector>
 using namespace std;
-
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
         int count=0;
-        for(auto word:words){
-          if(word.find(pref)==0)
-              count++;
+        int ans=0;
+        for(int i=0;i<words.size();i++)
+        {
+        
+            if(words[i].find(pref)==0)
+            {
+                cout<<"word ="<<words[i]<<endl;
+                count++;
+               
+            }
+         
         }
         return count;
     }
