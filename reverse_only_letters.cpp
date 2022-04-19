2cb68d4de9af5b301d508e3a1d333ceeb39f1cd4
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-04-19 19:02:39 +0530

    Add files via upload

--- reverse_only_letters.cpp
+++ reverse_only_letters.cpp
@@ -3,33 +3,36 @@
 All the characters that are not English letters remain in the same position.
 All the English letters (lowercase or uppercase) should be reversed.
 Return s after reversing it.*/
+
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-        
-    vector<char>x;
-    in
-    for(i=0;i<s.length();i++)
-    {
-        if((s[i]>='a' &&s[i]<='z') ||(s[i]>='A' &&s[i]<='Z') )
+        string str="";
+        for(int i=0;i<s.length();i++)
         {
-            x.push_back(s[i]);
+           if(s[i]>='a'&& s[i]<='z' || s[i]>='A' && s[i]<='Z' )
+           {
+               str+=s[i];
+               cout<<"str= "<<str<<endl;
+           }
+         
         }
-    }
-    cout<<"String before "<<s;
-    int k=x.size()-1;
-    for(i=0;i<s.length();i++)
-    {
-         if((s[i]>='a' &&s[i]<='z') ||(s[i]>='A' &&s[i]<='Z'))
+        reverse(str.begin(),str.end());
+        int j=0;
+        for(int i=0;i<s.length();i++)
         {
-            s[i]=x[k];
-            k--;
+           if(s[i]>='a'&& s[i]<='z' || s[i]>='A' && s[i]<='Z' )
+           {
+               s[i]=str[j];
+               
+               cout<<"str[j]= "<<str[j]<<' ';
+               j++;
             
+           }
         }
-
-    }
-    cout<<"String before "<<s;
-        return s;
         
+        return s;
     }
 };
