49316617f84592e66bc37d473c89e396bf40fc16
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-05-18 12:06:17 +0530

    Add files via upload

--- Count_Integers_With_Even_Digit_Sum.cpp
+++ Count_Integers_With_Even_Digit_Sum.cpp
@@ -1,28 +1,37 @@
+
 /*Given a positive integer num, return the number of positive integers less than or equal to num whose digit sums are even.
 
 The digit sum of a positive integer is the sum of all its digits.*/
 
-#include <iostream>
-#include <bits/stdc++.h>
-#include <vector>
+#include<bits\stdc++.h>
+#include<vector>
 using namespace std;
-
 
 class Solution {
 public:
     int countEven(int num) {
-        int sum=0,count=0;
-     for(int i=1;i<=num;i++){
-         int number=i;
-         while(number<=0){   
-             sum=sum+number%10;  
-             number=number/10;    
-         }
-         if(sum%2==0){
-             count++;
-         }
-         sum=0;
-     }
+        int count=0;
+        int last=0;
+        int sum=0;
+        for(int i=1;i<=num;i++)
+        {
+            int n=i;
+       while(n>0)
+       {
+           last=n%10;
+           sum+=last;
+           n=n/10;
+           cout<<"sum= "<<sum<<endl;
+       }
+            if(sum%2==0)
+            {
+                count++;
+            }
+             sum=0;
+        }
+        
+       
         return count;
     }
+    
 };
