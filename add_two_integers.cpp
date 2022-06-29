98cc4b43bd5dcce48db19b8f009f373c165dd21c
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2022-06-29 14:11:24 +0530

    Add files via upload

---
+++ add_two_integers.cpp
@@ -0,0 +1,18 @@
+/*Given two integers num1 and num2, return the sum of the two integers.*/
+
+
+
+#include<bits\stdc++.h>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        int total=0;
+        
+        total=num1+num2;
+        
+        return total;
+    }
+};
