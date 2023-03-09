31f3d2228e90cf9d7c5e0bd2bdc18e45fd5ad727
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2023-03-10 00:25:51 +0530

    Add files via upload

---
+++ single_num_II.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+             unordered_map<int,int>mp;
+               int ans=0;
+        for(int i =0;i<nums.size();i++){
+           mp[nums[i]]++;
+   
+        }
+        
+          for(auto it:mp)
+          {
+              if(it.second==1)
+              {
+                  ans=it.first;
+              }
+          }
+            
+        
+      return ans;
+    }
+};
